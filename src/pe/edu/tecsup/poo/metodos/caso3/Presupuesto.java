27334e8112d89f15a11781d3175b7d6197209c0e
Jaime <jgomezm@tecsup.edu.pe>
2021-07-06 20:57:05 -0500

    Update

--- src/pe/edu/tecsup/poo/metodos/caso3/Presupuesto.java
+++ src/pe/edu/tecsup/poo/metodos/caso3/Presupuesto.java
@@ -3,9 +3,9 @@ package pe.edu.tecsup.poo.metodos.caso3;
 // Ejercicio : usar la clase Departamento y crear el metodo
 //             calcularArea() , el metodo debe retonar
 //             los metros cuadrados 
-//             Debe crear la clase Presupuesto [ main()] donde calcular 
-//             el precio del Dpto sabiendo que el metro
-//             cuadrado esta 4000 soles
+//             Debe crear la clase Presupuesto [ main()] donde 
+//             debe calcular el precio del Dpto sabiendo que el 
+//             metro cuadrado esta 4000 soles
 
 public class Presupuesto {
 
