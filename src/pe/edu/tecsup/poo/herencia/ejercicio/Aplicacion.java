0bc070dc5f4b052178a5d1d75c8359bba0aa4ebd
Jaime <jgomezm@tecsup.edu.pe>
2021-07-08 19:59:43 -0500

    Exercise

---
+++ src/pe/edu/tecsup/poo/herencia/ejercicio/Aplicacion.java
@@ -0,0 +1,16 @@
+package pe.edu.tecsup.poo.herencia.ejercicio;
+/*
+Cuando se instancie una clase , el método información debe imprimir por consola lo siguiente:
+- Auto ==> "Auto generico"
+- AutoDeportivo ==> "Auto de lujo"
+- SUV ==> "Auto famliar"
+- Camioneta ==> "Auto generico"
+*/
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
