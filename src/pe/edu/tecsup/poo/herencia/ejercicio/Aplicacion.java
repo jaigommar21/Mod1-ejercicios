1ae51af80a3aaab96b9f4a27ac1782fd47813abf
Jaime <jgomezm@tecsup.edu.pe>
2021-07-08 20:17:51 -0500

    Udpate

--- src/pe/edu/tecsup/poo/herencia/ejercicio/Aplicacion.java
+++ src/pe/edu/tecsup/poo/herencia/ejercicio/Aplicacion.java
@@ -11,6 +11,9 @@ public class Aplicacion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Auto auto = new Auto();
+		auto.informacion();
+		
 	}
 
 }
---
+++ src/pe/edu/tecsup/poo/herencia/ejercicio/Auto.java
@@ -0,0 +1,12 @@
+package pe.edu.tecsup.poo.herencia.ejercicio;
+
+public class Auto {
+
+	private String marca;
+	private String anho;
+	
+	public void informacion() {
+		System.out.println("Auto generico");
+	}
+	
+}
