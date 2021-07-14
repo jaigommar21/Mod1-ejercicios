b9eb6e48fd803bf22e66cb1f7074f773c106303f
Jaime <jgomezm@tecsup.edu.pe>
2021-07-13 19:37:22 -0500

    Update

---
+++ src/pe/edu/tecsup/interfaces/concepto/ServiciosCompras.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.interfaces.concepto;
+
+public class ServiciosCompras {
+	
+	// Sinonimo de implementacion son las : {}
+	public void realizarPago() {
+		// TODO
+	}
+
+	public void realizarPedido() {
+		// TODO 
+	}
+}
