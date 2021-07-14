f4dbfb6990bdee2bfa85775a0c1315fbe7e9e021
Jaime <jgomezm@tecsup.edu.pe>
2021-07-13 19:40:24 -0500

    Update

---
+++ src/pe/edu/tecsup/interfaces/concepto/ServicioComprasInterface.java
@@ -0,0 +1,10 @@
+package pe.edu.tecsup.interfaces.concepto;
+
+public interface ServicioComprasInterface {
+
+	// Definicion de un comportamiento
+	public void realizarPedido();
+	
+	public void realizarPago();
+	
+}
