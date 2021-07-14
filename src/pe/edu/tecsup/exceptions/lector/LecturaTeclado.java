0b6d2321949c9d67ce394ba0a748b1a3bee0c49d
Jaime <jgomezm@tecsup.edu.pe>
2021-07-13 21:02:19 -0500

    Update

--- src/pe/edu/tecsup/exceptions/lector/LecturaTeclado.java
+++ src/pe/edu/tecsup/exceptions/lector/LecturaTeclado.java
@@ -1,17 +1,37 @@
 package pe.edu.tecsup.exceptions.lector;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LecturaTeclado {
 
 	public static void main(String[] args) {
 		
+		// Ingreso de texto
+		
 		System.out.print("Ingrese un texto : ");
 		Scanner sc = new Scanner(System.in);
 		String cadena = sc.nextLine();
 		
 		System.out.println("Mensaje ingresado : " + cadena);
+
 		
+		
+		// Ingreso de entero
+		
+		System.out.print("Ingrese un entero : ");
+		boolean flag = true;
+		while(flag)
+			try {
+				int entero = sc.nextInt();	// 12 (Enter)
+				flag = false;
+				System.out.println("Numero ingresado : " + entero);
+			} catch(InputMismatchException e) {
+				System.err.println("Debe ingresar un valor entero!");
+				sc.nextLine();
+			}
+		
+		sc.close();
 	}
 
 }
