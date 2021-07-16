25600f946151becd5f4c508cad7a4a0789f9a8de
Jaime <jgomezm@tecsup.edu.pe>
2021-07-15 19:33:28 -0500

    Update

---
+++ src/pe/edu/tecsup/uitl/string/EjercioString.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.uitl.string;
+
+public class EjercioString {
+		/*
+	    Crear un aplicacion para reemplazar la palabra
+	    "Jueves" por "Miercoles" , y luego imprimir
+	    todas las palabra de la frase por linea en mayuscula
+		 */
+		public static void main(String[] args) {
+			String s = "Hoy es Jueves por la noche";
+			
+		}
+}
