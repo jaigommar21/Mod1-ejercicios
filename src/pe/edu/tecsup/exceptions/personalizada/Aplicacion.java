61878903709ecb082173bdb77ad9dfe57bf09318
Jaime <jgomezm@tecsup.edu.pe>
2021-07-15 19:58:46 -0500

    Update

---
+++ src/pe/edu/tecsup/exceptions/personalizada/Aplicacion.java
@@ -0,0 +1,12 @@
+package pe.edu.tecsup.exceptions.personalizada;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		String nota = "12as" ; //"12as";
+		
+		int notaInt = Integer.parseInt(nota);
+		System.out.println(notaInt + 2);
+	}
+
+}
