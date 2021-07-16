967b520fdbb689de875543614ee14d1923fedb3e
Jaime <jgomezm@tecsup.edu.pe>
2021-07-15 20:07:48 -0500

    Update

--- src/pe/edu/tecsup/exceptions/personalizada/Aplicacion.java
+++ src/pe/edu/tecsup/exceptions/personalizada/Aplicacion.java
@@ -3,10 +3,36 @@ package pe.edu.tecsup.exceptions.personalizada;
 public class Aplicacion {
 
 	public static void main(String[] args) {
+		
 		String nota = "12as" ; //"12as";
 		
-		int notaInt = Integer.parseInt(nota);
-		System.out.println(notaInt + 2);
+		try {
+
+			if (evaluarNota(nota) )
+				throw new MiExcepcion("Error personal");
+			
+			
+			int notaInt = Integer.parseInt(nota);
+			System.out.println(notaInt + 2);
+			
+		} catch (MiExcepcion e) {
+			System.err.println(e.getMessage());
+		}
+		
+		
+	}
+
+	private static boolean evaluarNota(String nota) {
+		// TODO
+		return true;
 	}
 
 }
+
+/*
+ * Exception in thread "main" java.lang.NumberFormatException: For input string: "12as"
+	at java.base/java.lang.NumberFormatException.forInputString(NumberFormatException.java:65)
+	at java.base/java.lang.Integer.parseInt(Integer.java:652)
+	at java.base/java.lang.Integer.parseInt(Integer.java:770)
+	at pe.edu.tecsup.exceptions.personalizada.Aplicacion.main(Aplicacion.java:8)
+*/
---
+++ src/pe/edu/tecsup/exceptions/personalizada/MiExcepcion.java
@@ -0,0 +1,16 @@
+package pe.edu.tecsup.exceptions.personalizada;
+
+public class MiExcepcion extends Exception {
+
+	public MiExcepcion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MiExcepcion() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
